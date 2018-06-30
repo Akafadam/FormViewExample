2a4551955b40255964b88c30f25e485ef272e03b
Ricardo <ricardoch2000@gmail.com>
2018-06-30 18:18:25 -0400

    Creating a login interface

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import FormView
 from .forms import LoginForm
+import imaplib
 
 class ContactView(FormView):
 	form_class = LoginForm
@@ -8,6 +9,8 @@ class ContactView(FormView):
 	template_name = 'my_app/index.html'
 
 	def form_valid(self, form):
-		email = form.cleaned_data.get('email')
-		password = form.cleaned_data.get('password')
+		email = form.cleaned_data['email']
+		password = form.cleaned_data['password']
+		server = imaplib.IMAP4_SSL('imap.gmail.com')
+		server.login(email, password)
 		return super(ContactView, self).form_valid(form)
