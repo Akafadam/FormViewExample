ce51586fbad5b44904de660dbc8e8fcef9fcdff8
Ricardo <ricardoch2000@gmail.com>
2018-06-29 21:31:59 -0400

    FormView finished

--- my_app/urls.py
+++ my_app/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
-urlspatterns = [
+urlpatterns = [
 	path('', views.ContactView.as_view(), name='contact'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import FormView
+from .forms import LoginForm
 
-# Create your views here.
+class ContactView(FormView):
+	form_class = LoginForm
+	success_url = '/thanks/'
+	template_name = 'my_app/index.html'
+
+	def form_valid(self, form):
+		email = form.cleaned_data.get('email')
+		password = form.cleaned_data.get('password')
+		return super(ContactView, self).form_valid(form)
