0ad97945a77f71e06ef32c5fc92225955d4d5a1a
Ricardo <ricardoch2000@gmail.com>
2018-07-03 00:46:25 -0400

    The password was too short

--- my_app/views.py
+++ my_app/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import FormView
 from .forms import LoginForm
 import imaplib
 
 class ContactView(FormView):
-	form_class = LoginForm
+	form_class = UserCreationForm
 	success_url = '/thanks/'
 	template_name = 'my_app/index.html'
 
 	def form_valid(self, form):
-		email = form.cleaned_data['email']
-		password = form.cleaned_data['password']
-		server = imaplib.IMAP4_SSL('imap.gmail.com')
-		server.login(email, password)
+		username = form.cleaned_data['username']
+		password = form.cleaned_data['password1']
+		# server = imaplib.IMAP4_SSL('imap.gmail.com')
+		# server.login(email, password)
+		user = authenticate(username=username, password=password)
+		login(request, user)
+
 		return super(ContactView, self).form_valid(form)
