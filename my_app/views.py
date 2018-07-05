80c8b5b4f628f92a9a874e81b6988b7d80d4a2d0
Ricardo <ricardoch2000@gmail.com>
2018-07-04 23:36:54 -0400

    Changes made to the views

--- my_app/views.py
+++ my_app/views.py
@@ -1,20 +1,17 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import FormView
 from .forms import LoginForm
-import imaplib
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import User, UserCreationForm
 
 class ContactView(FormView):
-	form_class = UserCreationForm
+	form_class = LoginForm
 	success_url = '/thanks/'
 	template_name = 'my_app/index.html'
 
 	def form_valid(self, form):
 		username = form.cleaned_data['username']
 		password = form.cleaned_data['password1']
-		# server = imaplib.IMAP4_SSL('imap.gmail.com')
-		# server.login(email, password)
-		user = authenticate(username=username, password=password)
+		user = authenticate(username=username, email=email, password=password)
 		login(request, user)
-
 		return super(ContactView, self).form_valid(form)
